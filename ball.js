b6ced8dc8858cfeb71e848e4b75ddfc891317a5b
Leo Au-Yeung <leo297747@gmail.com>
2018-12-14 01:39:40 -0500

    Created js classes for all objects

---
+++ ball.js
@@ -0,0 +1,43 @@
+// Class for one ball in the game
+class Ball {
+	//Constructor
+	constructor(canvas) {
+		this.canvas = canvas;
+		this.RADIUS = 5; //RADIUS (convention: CAPITALCASE = variable you won't change
+		
+		this.position = canvas.getStartPosition(); //Initialize position variable
+		this.movement = new Movement(0, 0); //Initialize movement variable
+		
+		//this.dx = Math.cos(angle);
+		//this.dy = Math.sin(angle);
+	}
+	
+	// Returns whether the ball is currently moving.
+	// All balls do not necesserily move at the beginning of the game.
+	isMoving() { return this.movement.isMoving() }
+	
+	//checkMove - checks movement of ball
+	//If ball has hit a tile, updates tile and bounces off in opposite direction
+	//If ball has hit a wall, bounces off in opposite direction
+	createMovement() {
+		return new BallMovement(this.position, this.movement);
+	}
+		
+	//draw - Draws a ball using its current position
+	draw() {
+		this.canvas.setColor("#DDDDDD"); //Color = black
+		this.canvas.draw().beginPath();
+		this.canvas.draw().arc(this.position.getX(), this.position.getY(), this.RADIUS, 0, 2 * Math.PI);
+		this.canvas.draw().fill();
+		console.log('DRAWING BALL');
+	}
+
+	//move - Moves ball by updating xcor and ycor with dx and dy
+	//If ball has hit a wall, bounces off in opposite direction
+	move(movement) {
+		this.movement = movement;
+		this.position.move(movement); //position.x += dx; position.y += dy;
+		//console.log('X-pos:', this.position.getX(), ' Y-pos:', this.position.getY());
+		
+	}
+}
---
+++ main.js
@@ -0,0 +1,90 @@
+$(function() {
+	
+	var canvas = new Canvas();
+	var running = false; //game running flag
+	var startTime; //start time
+	
+	//Ball movement
+	var mvmt;
+	
+	//Create the ball launcher
+	var launcher = new BallLauncher(canvas);
+	
+	//Create the balls
+	var balls = [];
+	for (var i = 0; i < 10; i++) {
+		balls.push( new Ball(canvas) );
+	}
+	
+	//Create the tiles
+	var tiles = [];
+	for (var j = 0; j < 5; j++) {
+		tiles.push( new Tile (canvas, j) );
+	}
+	
+	// Makes all the balls progress by one step.
+	var step = function() {
+		console.log('Running game');
+			
+		canvas.draw().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+		for (var j = 0; j < tiles.length; j++) {
+			tiles[j].draw();
+		}
+		for (var i = 0 ; i < balls.length ; i++) {
+			var ball = balls[i];
+			
+			ball.draw();
+			// Check if the ball should start moving.
+			 if (!ball.isMoving()) {
+				if (Date.now() - startTime > 500 * i) {
+					ball.move(mvmt);
+				}
+				ball.draw();
+				return;
+			}
+
+			var move = ball.createMovement();
+			move.handleBorder(canvas);
+			for (var j = 0; j < tiles.length; j++) {
+				var tile = tiles[j];
+				
+				if (move.handleTile(tile)) {
+					tile.touchedByBall();
+					tile.draw();
+				}
+				
+			}
+			ball.move(move.getMovement());
+			ball.draw();
+		}
+	};
+	
+	//Point launcher
+	onmousemove = function(event)  {
+		if (!running) {
+			canvas.draw().clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
+			canvas.draw();
+			for (var j = 0; j < tiles.length; j++) {
+				tiles[j].draw();
+			}
+			launcher.draw(event); //Points launcher upon mouse hover (right now just creates a ball on cursor)
+		}
+	}
+	
+	//Shoot balls
+	onclick = function(e) {
+		running = true;
+		console.log('Clicked mouse');
+		var mouseX = e.clientX;
+		var mouseY = e.clientY;
+		//if (mouseX > canvas.getStartPosition().getX()) { mouseX = canvas.getStartPosition().getX(); }
+		if (mouseY > canvas.getStartPosition().getY()) { mouseY = canvas.getStartPosition().getY() - 45; }
+		var angle = Math.atan2(mouseY - canvas.getStartPosition().getY(), 
+				mouseX - canvas.getStartPosition().getX());
+		mvmt = new Movement(Math.cos(angle), Math.sin(angle));
+		startTime = Date.now();
+		setInterval(step, 10);
+	}
+
+});
+
