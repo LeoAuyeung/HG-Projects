f7078ceee5ba54231a9fa1b7de0ca6acc0e8c2ea
Leo <leo297747@gmail.com>
2019-01-15 13:21:05 -0500

    Fixed balls

--- main.js
+++ main.js
@@ -73,12 +73,13 @@ $(function() {
 	//Checks whether all balls have stopped (reached bottom of screen)
 	var checkBallsStopped = function() {
 		//If any balls are still moving, then balls have not stopped
-		for (var i = 0; i < score; i++) {
-			let ball = balls[i];
+		//Commenting out for loop for now as there is only 1 ball
+		//for (var i = 0; i < score; i++) {
+			let ball = balls[0];
 			if (ball.movement.getDx() != 0 && ball.movement.getDy() != 0) {
 				return false;
 			}
-		}
+		//}
 		//Otherwise, return true
 		return true;
 	}
@@ -94,6 +95,7 @@ $(function() {
 				if (tile.isActive && tile.getTop() >= canvas.getHeight() - 99) {
 					console.log("Game over");
 					console.log("Refresh page to try again");
+					//Currently does not "end" game yet
 				}
 			}
 		}
